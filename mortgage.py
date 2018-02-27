63a25297ea32aa1daf0749729b83f530fb490688
Samarth <samarth.1397@gmail.com>
2018-02-27 23:30:56 +0530

    minor edit in mortgage.py

--- mortgage.py
+++ mortgage.py
@@ -1,23 +1,24 @@
 class State():
     
-    def __init__(self, fixed_interest_rate, variable_fraction, variable_interest_rate, number_of_payments):
-        self.fixed_interest_rate = fixed_interest_rate
+    def __init__(self, base_interest_rate, variable_fraction, variable_interest_rate, number_of_payments):
+        self.base_interest_rate = base_interest_rate
         self.variable_fraction = variable_fraction
         self.variable_interest_rate = variable_interest_rate
         self.number_of_payements = number_of_payements
     
     def __hash__(self):
-        return hash((self.fixed_interest_rate, self.variable_fraction, self.variable_interest_rate, self.number_of_payments))
+        return hash((self.base_interest_rate, self.variable_fraction, self.variable_interest_rate, self.number_of_payments))
 
 class Mortgage():
     
     def __init__(self):
-        self.principle_balance = 0.0
+        self.principle_balance = 0
         self.total_payments = 60
         self.days = 0
         self.coupon_payment = 0
         
-        self.fixed_interest_rate = 0.0
+        self.base_interest_rate=0.0                               
+        self.fixed_interest_rate = self.base_interest_rate+0.005 #m=rt+b
         self.variable_fraction = 0.0
         self.variable_interest_rate = 0.0
         self.number_of_payements = 0
@@ -39,6 +40,8 @@ class Mortgage():
             return None
     
     def state(self):
-        return State(self.fixed_interest_rate, self.variable_fraction, self.variable_interest_rate, self.number_of_payments)
+        return State(self.base_interest_rate, self.variable_fraction, self.variable_interest_rate, self.number_of_payments)
     
 
+    def recalculate_principal_balance(self):
+        return
